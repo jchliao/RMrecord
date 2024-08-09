ff1034bd9eaec3489069da519e160a906b7ec31f
Jinch <3318233133@qq.com>
2024-08-09 10:38:24 +0800

    gui update

--- record.py
+++ record.py
@@ -1,75 +1,99 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import ttk
 import subprocess
 import os
 from datetime import datetime
+import urllib.request
+import json
+
+json_url = 'https://pro-robomasters-hz-n5i3.oss-cn-hangzhou.aliyuncs.com/live_json/live_game_info.json'
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(script_dir,'live_data.json')
 ffmpeg_path = os.path.join(script_dir,'bin','ffmpeg.exe')
+
+def update_json():
+    with urllib.request.urlopen(json_url) as response:
+        response_data = response.read()
+        data = json.loads(response_data)
+        data = data['eventData'][0]
+        # 组成新的 JSON 对象
+        new_data = {
+            'zoneName': data['zoneName'],
+            'zoneLiveString': data['zoneLiveString'],
+            'fpvData': data['fpvData']
+        }
+        # 保存到文件
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(new_data, f, ensure_ascii=False, indent=4)
+
 
 # 下载任务列表
 processes = []
 downloading = False
-url_file = ""
 
 def start_stop_downloads():
-    global processes, downloading
+    global downloading
     if downloading:
         stop_downloads()
-        download_button.config(text="开始下载")
+        download_button.config(text="开始录制")
     else:
         start_downloads()
-        download_button.config(text="停止下载")
+        download_button.config(text="停止录制")
     downloading = not downloading
 
+def find_src_by_label(data, label):
+    for item in data:
+        if item['label'] == label:
+            return item['src']
+    return None
+
+def file_list():
+    global team_color,resolution_combobox
+    files = []
+    color = team_color.get()
+    resolution = resolution_combobox.get()
+    with open(json_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        zonelive = data['zoneLiveString']
+        src = find_src_by_label(zonelive,resolution)
+        files.append(('主视角',src))
+        for fpv in data['fpvData']:
+            if color in fpv['role']:
+                src = find_src_by_label(fpv['sources'],resolution)
+                files.append((fpv['role'],src))
+    return files
+
+
 def start_downloads():
-    global processes
-    urls = []
-    
-    # 从文件中读取 URL 列表
-    with open(url_file, 'r') as file:
-        urls = file.readlines()
-    
-    # 去除每行末尾的换行符
-    urls = [url.strip() for url in urls]
-    
+    global processes,files
     # 创建并行下载的 subprocess 任务
     processes = []
+    files = file_list()
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     output_folder = os.path.join('output', timestamp)
     os.makedirs(output_folder, exist_ok=True)
 
-    for url in urls:
-        # 提取文件名部分
-        filename_with_extension = url.split('/')[-1].split('?')[0]
-        base_name = filename_with_extension.split('.')[0]
-        output_file = base_name+'.mp4'
-        cmd = [ffmpeg_path,'-i', url, '-c','copy',os.path.join(output_folder, output_file)]
+    for file in files:
+        output_file = file[0]+'.mp4'
+        cmd = [ffmpeg_path,'-i', file[1], '-c','copy',os.path.join(output_folder, output_file)]
         process = subprocess.Popen(cmd,stdin=subprocess.PIPE)
         processes.append(process)
 
 def stop_downloads():
     global processes
     for process in processes:
-        # 发送 'q' 键命令到 ffmpeg 进程
-        process.stdin.write(b'q')
-        process.stdin.flush()
+        # 如果还在运行，发送 'q' 键命令到 ffmpeg 进程
+        if process.poll() is None:
+            process.stdin.write(b'q')
+            process.stdin.flush()
     # 等待所有子进程完成
     for process in processes:
         process.wait()
     print("所有下载任务已停止。")
     processes = []
 
-def select_file():
-    global url_file
-    url_file = filedialog.askopenfilename(title="选择 URL 文件", filetypes=[("Text Files", "*.txt")])
-    if url_file:
-        file_label.config(text=f"已选择文件: {os.path.basename(url_file)}")
-        download_button.config(state=tk.NORMAL)  # 启用下载按钮
-    else:
-        file_label.config(text="尚未选择文件")
-        download_button.config(state=tk.DISABLED)  # 禁用下载按钮
 
 def on_closing():
     if downloading:
@@ -80,23 +104,34 @@ def on_closing():
 # 创建主窗口
 root = tk.Tk()
 root.title("record")
-
-# 设置窗口大小 (宽 x 高)
-root.geometry("250x200")
 root.resizable(width=False, height=False)
 
-# 创建并放置按钮
+# 创建菜单栏
+menu_bar = tk.Menu(root)
+root.config(menu=menu_bar)
+menu_bar.add_command(label="更新 JSON", command=update_json)
+
 frame = tk.Frame(root)
-frame.pack(expand=True, fill=tk.BOTH)
+frame.grid(row=0, column=0, padx=25, pady=10,sticky='nsew')  # 使用 grid 布局
 
-select_button = tk.Button(frame, text="选择 URL 文件", command=select_file)
-select_button.pack(expand=True)
+# 创建下拉选择控件
+resolution_combobox = ttk.Combobox(frame, values=["540p", "720p", "1080p"])
+resolution_combobox.current(1)  # 将当前选项设置为 720p
+resolution_combobox.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
 
-download_button = tk.Button(frame, text="开始下载", command=start_stop_downloads, state=tk.DISABLED)
-download_button.pack(expand=True)
+# 创建一个 Tkinter 变量用于存储选中的值
+team_color = tk.StringVar()
+team_color.set("红")  # 设置默认选项
 
-file_label = tk.Label(frame, text="尚未选择文件")
-file_label.pack(expand=True)
+# 创建两个 Radiobutton 控件
+radio1 = tk.Radiobutton(frame, text="红方", variable=team_color, value="红")
+radio1.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
+
+radio2 = tk.Radiobutton(frame, text="蓝方", variable=team_color, value="蓝")
+radio2.grid(row=1, column=1, padx=5, pady=5, sticky='ew')
+
+download_button = tk.Button(frame, text="开始录制", command=start_stop_downloads)
+download_button.grid(row=2, column=0, columnspan=2,padx=10,pady=10, sticky='ew')
 
 # 设置窗口关闭时的处理函数
 root.protocol("WM_DELETE_WINDOW", on_closing)
